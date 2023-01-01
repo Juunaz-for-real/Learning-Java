3862b6a8625733e145749beacf69060cc490522f
Raick Miranda <mirandaraick@gmail.com>
2023-01-01 17:51:56 -0300

    COD1000

---
+++ beecrowd/iniciante/solutions/COD1000.java
@@ -0,0 +1,12 @@
+/* 
+ * → This algorithm was created by Raick Miranda (Juunaz-for-real) on january 1st, 2023.
+ * → Copyright (c) 2022 Raick Miranda - MIT License.
+ * 
+ * → Fun fact: while coding I was listening to Harry Styles album "Harry's House" ◠.◠
+ */
+
+public class COD1000 {
+    public static void main(String[] args) throws Exception {
+        MyIO.println("Hello World!");
+    }
+}
