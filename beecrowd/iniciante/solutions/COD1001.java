4a52c0a051673121c92f303d6ff328d687ffbed4
Raick Miranda <mirandaraick@gmail.com>
2023-01-01 18:05:20 -0300

    COD1001

---
+++ beecrowd/iniciante/solutions/COD1001.java
@@ -0,0 +1,15 @@
+/* 
+ * → This algorithm was created by Raick Miranda (Juunaz-for-real) on january 1st, 2023.
+ * → Copyright (c) 2022 Raick Miranda - MIT License.
+ * 
+ * → Fun fact: while coding I was listening to The Neighbourhood album "Wiped Out!" ◠.◠
+ */
+
+public class COD1001 {
+    public static void main(String[] args) throws Exception {
+        int A = MyIO.readInt(), B = MyIO.readInt();
+        int X = A + B;
+
+        MyIO.println("X = " + X);
+    }
+}
